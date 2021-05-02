cc4e4a4b2a8548b80333a03366c03d50aba9322e
Diego <diegomaciel211@gmail.com>
2021-05-01 23:50:20 -0300

    2 entrega

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
-
 //Declaraciones generales
 const contenedorProductos = document.getElementById('contenedor-productos')
 let carrito = []
+let contadorLista = []
 const contenedorCarrito = document.getElementById('carrito-contenedor')
 const precioTotal = document.getElementById('precioTotal')
 const contadorCarrito = document.getElementById('contadorCarrito')
 
-let carritoEnLS = JSON.parse( localStorage.getItem('carrito') )
-let contadorEnLs = JSON.parse( localStorage.getItem('contador') )
-let contenedorEnLs = JSON.parse( localStorage.getItem('contenedor') )
-let totalEnLs = JSON.parse( localStorage.getItem('total') )
+
+let carritoEnLS = JSON.parse(localStorage.getItem('carrito'))
+let contadorEnLs = JSON.parse(localStorage.getItem('contador'))
+let contenedorEnLs = JSON.parse(localStorage.getItem('contenedor'))
+let totalEnLs = JSON.parse(localStorage.getItem('total'))
+let contadorListaEnLS = JSON.parse(localStorage.getItem('contadorLista'))
+
+
+
+//¿Continuar compra?
 
 if (carritoEnLS) {
     let continuar = confirm('Usted tiene productos agregados al carrito ¿Desea continuar con su última compra?')
@@ -19,10 +25,11 @@ if (carritoEnLS) {
         contadorCarrito.innerText = contadorEnLs
         contenedorCarrito.innerText = contenedorEnLs
         precioTotal.innerText = totalEnLs
-        
+        contadorLista = contadorListaEnLS
+
     } else {
         localStorage.removeItem('carrito')
-     }
+    }
 }
 
 
@@ -35,18 +42,18 @@ function mostrarProductos(array) {
     contenedorProductos.innerHTML = ''
 
     array.forEach((producto) => {
-    
+
         let div = document.createElement('div')
         div.classList.add('producto')
         div.classList.add('container-fluid')
-              div.innerHTML = `
+        div.innerHTML = `
                     <img class="img-fluid" src=${producto.imagen} alt="">
                     <h3>${producto.descripcion}</h3>
                     <p class="precioProducto">Precio: $${producto.precio}</p>
-                    <div class="container-btn-del"><button onclick=agregarAlCarrito(${producto.id}) class="boton-agregar center">Agregar <i class="fas fa-cart-plus"></i></button></div>
+                    <div class="container-btn-del"><button onclick=agregarAlCarrito(${producto.id}) class="boton-agregar btn btn-danger center">Agregar <i class="fas fa-cart-plus"></i></button></div>
         `
         contenedorProductos.appendChild(div)
-    
+
     })
 
 }
@@ -58,18 +65,38 @@ mostrarProductos(productos)
 //Agregar y actualizar el carrito
 
 function agregarAlCarrito(id) {
-    const productoElegido = productos.find( el => el.id == id )
+
+    const productoElegido = productos.find(el => el.id == id)
+
+
+
     if (productoElegido) {
-        carrito.push(productoElegido)   
+        contadorLista.push(productoElegido)
+        const repetido = carrito.find(el => el.id == productoElegido.id)
+        const indice = carrito.indexOf(repetido)
+        console.log(indice)
+
+        //Si se repite solo sumar cantidad
+        if (repetido) {
+            carrito[indice].cantidad++
+            console.log(carrito);
+
+        }
+        //Si no se repite agregar al carrito con cantidad 1
+        else {
+            productoElegido.cantidad = 1
+            carrito.push(productoElegido)
+        }
     }
-   
+
     actualizarCarrito()
 }
 
 function eliminarProducto(id) {
-    const productoEliminar = carrito.find( el => el.id == id )
+    const productoEliminar = carrito.find(el => el.id == id)
     const indice = carrito.indexOf(productoEliminar)
     carrito.splice(indice, 1)
+    contadorLista.splice(indice)
 
     actualizarCarrito()
 }
@@ -79,55 +106,74 @@ function eliminarProducto(id) {
 
 
 function actualizarCarrito() {
-            
+
     localStorage.setItem('carrito', JSON.stringify(carrito))
-           console.log(contadorCarrito)
+
+
+    contenedorCarrito.innerHTML = ''
 
 
 
-    contenedorCarrito.innerHTML = '' 
+
 
     carrito.forEach((producto) => {
+
+
+
         contenedorCarrito.innerHTML += `
             <div class="productoEnCarrito">
                 <p>${producto.descripcion}</p>
                 <img class="img-fluid" style="max-height:100px" src=${producto.imagen} alt="">
                 <p>Precio: $${producto.precio}</p>
-                <button onclick=eliminarProducto(${producto.id})  class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+                <p>Cantidad: ${producto.cantidad}</p>
+                <button onclick=eliminarProducto(${producto.id})  class="boton-eliminar btn btn-secondary"><i class="fas fa-trash-alt"></i></button>
             </div>
         `
-    })
- 
 
-    precioTotal.innerText = carrito.reduce( (acc, el) => acc += el.precio, 0 )
-    if (carrito.length!==0){
-    contadorCarrito.innerText = carrito.length
-}else{
-    contadorCarrito.innerText = "0"
-}
+    })
+
+
+    precioTotal.innerText = carrito.reduce((acc, el) => acc += el.precio, 0)
+
+
+
+    if (contadorLista.length !== 0) {
+        contadorCarrito.innerText = contadorLista.length
+    } else {
+        contadorCarrito.innerText = "0"
+    }
 
     localStorage.setItem('contenedor', JSON.stringify(contenedorCarrito.innerText))
     localStorage.setItem('total', JSON.stringify(precioTotal.innerText))
     localStorage.setItem('contador', JSON.stringify(contadorCarrito.innerText))
+    localStorage.setItem('contadorLista', JSON.stringify(contadorLista))
 
-   
-  
- }
+
+
+}//Boton Promo
+const botonPromo = document.getElementById('boton-promo')
+
+function promo(id){
+    let promo = productos.filter(el => el.id === id )
+    mostrarProductos(promo)
+}
 
 
 //Filtrar por precios
 
 const ordenPrecios = document.getElementById('precios')
 
-ordenPrecios.addEventListener('change', ()=>{
+ordenPrecios.addEventListener('change', () => {
     if (ordenPrecios.value == 1) {
-        productos.sort( (a, b) => a.precio - b.precio )
+    
+        productos.sort((a, b) => a.precio - b.precio)
         mostrarProductos(productos)
     } else if (ordenPrecios.value == 2) {
-        productos.sort( (a, b) => b.precio - a.precio )
+        productos.sort((a, b) => b.precio - a.precio)
         mostrarProductos(productos)
     } else {
-        productos.sort( (a, b) => a.id - b.id )
+        productos.sort((a, b) => a.id - b.id)
+        productos = productosOriginal
         mostrarProductos(productos)
     }
 })
@@ -135,14 +181,35 @@ ordenPrecios.addEventListener('change', ()=>{
 
 //Filtrar por tipo de producto
 
+const tipoProducto = document.getElementById('tipo-producto')
+
+tipoProducto.addEventListener('change', () => {
+   
+
+    if (tipoProducto.value === "all") {
+        productos = productosOriginal
+             mostrarProductos(productos)}
+
+        else{
+            productos = productosOriginal
+            let filtrado = productos.filter(el => el.tipo === tipoProducto.value )
+            productos = filtrado
+                     mostrarProductos(filtrado)
+        }
+    
+})
+
+
+
 //Borrar carrito
 
-function borrarCarrito(){
+function borrarCarrito() {
     localStorage.clear()
     contadorCarrito.innerText = '0'
-    contenedorCarrito.innerHTML= ''
+    contadorLista = []
+    contenedorCarrito.innerHTML = ''
     precioTotal.innerText = ''
     carrito = []
     alert('Su carrito ha sido eliminado')
-           }
-
+    console.log(carrito, carritoEnLS)
+}
